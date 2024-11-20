6a10be39e122fc9b5097cea869429c858186aef8
shadoweeq <szymonwylegala@gmail.com>
2024-11-20 21:03:39 +0100

    template strona

---
+++ header-footer-loader.js
@@ -0,0 +1,29 @@
+class ExoticaHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+        <header>
+        <ul>
+            <li><a href="index.html">Strona głowna</a></li>
+            <li><a href="sklep.html">Płazy</a></li>
+            <li><a href="sklep.html">Gady</a></li>
+            <li><a href="sklep.html">Stawonogi</a></li>
+            <li><a href="sklep.html">Ryby</a></li>
+            <li><a href="#contact">Kontakt</a></li>
+        </ul>
+        </header>
+        `;
+  }
+}
+
+class ExoticaFooter extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+        <footer>
+        <p>&copy; Copyright 2024 Exotica</p>
+        </footer>
+    `;
+  }
+}
+
+customElements.define("exotica-header", ExoticaHeader);
+customElements.define("exotica-footer", ExoticaFooter);
