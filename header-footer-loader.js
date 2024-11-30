0e2a0c4603f8fe2cb8f0fc168ef45a822fce48b6
shadoweeq <szymonwylegala@gmail.com>
2024-11-30 17:28:12 +0100

    zmieniony nav bar i rozwijane menu sklepu

--- header-footer-loader.js
+++ header-footer-loader.js
@@ -9,8 +9,7 @@ class ExoticaHeader extends HTMLElement {
         <header>
         <nav>
             <ul class='nav-bar'>
-                <li class='logo'><a href='index.html'><img src='./img/favicon.ico'/></a></li>
-                	
+                <li class='logo'><a href='index.html'><img src='img/exotica_logo.png'></a></li>
                 <input type='checkbox' id='check' />
                 <span class="menu">
                     <div class="theme-switch">
@@ -21,19 +20,95 @@ class ExoticaHeader extends HTMLElement {
                       <div class="theme-ball"></div>
                     </label>
                   </div>
-                    <li><a href="index.html">Strona główna</a></li>
-                    <li><a href="konfigurator.html">Konfigurator</a></li>
-                    <li><a href="sklep.html">Płazy</a></li>
-                    <li><a href="sklep.html">Gady</a></li>
-                    <li><a href="sklep.html">Stawonogi</a></li>
-                    <li><a href="sklep.html">Ryby</a></li>
-                    <li><a href="#" id="toggle-cart">Koszyk</a></li>
+                    <li class="home"><a href="index.html"><i class="fa-solid fa-house fa-xl"></i></a></li>
+                    <li><a href="konfigurator.html"><i class="fa-solid fa-sliders fa-xl"></i></a></li>
+                    <li><a href="konto.html"><i class="fa-solid fa-user fa-xl"></i></a></li>
+                    <li><a href="#" id="toggle-cart"><i class="fa-solid fa-cart-shopping fa-xl"></i></a></li>
                     <label for="check" class="close-menu"><i class="fas fa-times"></i></label>
                 </span>
                 <label for="check" class="open-menu"><i class="fas fa-bars"></i></label>
             </ul>
         </nav>
         </header>
+        <nav class="shop-menu">
+          <ul class='shop-nav-bar'>
+              <span class="menu">
+                </div>
+
+                  <li><a href="#">Zwierzęta <i class="fas fa-caret-down"></i></a>
+                    <div class="dropdown-menu">
+                        <ul>
+                          <li>
+                            <a href="#">Płazy <i class="fas fa-caret-right"></i></a>
+                            <div class="dropdown-menu-1">
+                              <ul>
+                                <li><a href="#">Żaby</a></li>
+                                <li><a href="#">Salamandry</a></li>
+                              </ul>
+                            </div>
+                          </li>
+                          <li>
+                            <a href="#">Gady <i class="fas fa-caret-right"></i></a>
+                            <div class="dropdown-menu-1">
+                              <ul>
+                                <li><a href="#">Jaszczurki</a></li>
+                                <li><a href="#">Węże</a></li>
+                                <li><a href="#">Żółwie</a></li>
+                              </ul>
+                            </div>
+                          </li>
+                          <li>
+                            <a href="#">Stawonogi <i class="fas fa-caret-right"></i></a>
+                            <div class="dropdown-menu-1">
+                              <ul>
+                                <li><a href="#">Pająki</a></li>
+                                <li><a href="#">Skorpiony</a></li>
+                                <li><a href="#">Owady</a></li>
+                              </ul>
+                            </div>
+                          </li>
+                          <li>
+                            <a href="#">Ryby <i class="fas fa-caret-right"></i></a>
+                            <div class="dropdown-menu-1">
+                              <ul>
+                                <li><a href="#">Słodkowodne</a></li>
+                                <li><a href="#">Morskie</a></li>
+                                <li><a href="#">Denne</a></li>
+                              </ul>
+                            </div>
+                          </li>
+                        </ul>
+                      </div>
+                  </li>
+
+                  <li><a href="">Karma</a></li>
+
+                  <li><a href="#">Terraria <i class="fas fa-caret-down"></i></a>
+                    <div class="dropdown-menu">
+                        <ul>
+                          <li>
+                            <a href="konfigurator.html"><i class="fa-solid fa-sliders"></i> Konfigurator</a>
+                          </li>
+                          <li>
+                            <a href="#">Terraria</a>
+                          </li>
+                          <li>
+                            <a href="#">Akwaria</a>
+                          </li>
+                          <li>
+                            <a href="#">Dekoracje</a>
+                          </li>
+                          <li>
+                            <a href="#">Akcesoria</a>
+                          </li>
+                        </ul>
+                      </div>
+                  </li>
+                  <li><a href="">Akcesoria</a></li>
+              </span>
+              <label for="check" class="open-menu"><i class="fas fa-bars"></i></label>
+          </ul>
+        </nav>
         `;
   }
 }
