e23d17dcca17f5a5ffc14960957b3930dd4c0dc0
tomeczeklmaooo <tomeczeklmao@proton.me>
2024-12-01 13:38:23 +0100

    coś tam z konfiguratorem zrobiłem (tyle co nic)

--- konfigurator.js
+++ konfigurator.js
@@ -2,13 +2,17 @@ function generuj()
 {
 	var tekst = document.getElementById('text_grawer').value;
 	var rozmiar = document.getElementsByName('rozmiar_terrarium');
+	var teren = document.getElementById('rodzaj_terenu').value;
 	var dodatki = document.getElementsByName('dodatek');
 
 	var preview_container = document.getElementById('cfg-preview');
 
 	var terrarium_anim = 'terrarium_in 1s ease';
 
-	var kwadrat = document.createElement('div');
+	var terrarium = document.createElement('img');
+	terrarium.src = 'img/terrarium.png';
+
+	var grawer = document.createElement('span');
 
 	var dodatki_wybrane = [];
 
@@ -31,41 +35,33 @@ function generuj()
 	switch (rozmiar)
 	{
 		case 'SMALL':
-			preview_container.removeChild(preview_container.children[0]);
-			preview_container.appendChild(kwadrat);
-			kwadrat.style.animation = terrarium_anim;
-			kwadrat.innerHTML = tekst;
-			kwadrat.style.width = '180px';
-			kwadrat.style.height = '120px';
+			dodaj_terrarium(preview_container, terrarium, terrarium_anim, grawer, tekst, '180px', '120px');
 			break;
 		case 'MEDIUM':
-			preview_container.removeChild(preview_container.children[0]);
-			preview_container.appendChild(kwadrat);
-			kwadrat.style.animation = terrarium_anim;
-			kwadrat.innerHTML = tekst;
-			kwadrat.style.width = '300px';
-			kwadrat.style.height = '150px';
+			dodaj_terrarium(preview_container, terrarium, terrarium_anim, grawer, tekst, '300px', '200px');
 			break;
 		case 'LARGE':
-			preview_container.removeChild(preview_container.children[0]);
-			preview_container.appendChild(kwadrat);
-			kwadrat.style.animation = terrarium_anim;
-			kwadrat.innerHTML = tekst;
-			kwadrat.style.width = '450px';
-			kwadrat.style.height = '240px';
+			dodaj_terrarium(preview_container, terrarium, terrarium_anim, grawer, tekst, '400px', '300px');
 			break;
 		case 'EXTRALARGE':
-			preview_container.removeChild(preview_container.children[0]);
-			preview_container.appendChild(kwadrat);
-			kwadrat.style.animation = terrarium_anim;
-			kwadrat.innerHTML = tekst;
-			kwadrat.style.width = '600px';
-			kwadrat.style.height = '300px';
+			dodaj_terrarium(preview_container, terrarium, terrarium_anim, grawer, tekst, '600px', '400px');
 			break;
 	}
 
 	console.log(tekst);
 	console.log(rozmiar);
+	console.log(teren);
 	console.log(dodatki);
 	console.log(dodatki_wybrane);
+}
+
+function dodaj_terrarium(_container, _append, _anim, _fgtxt, _text, _width, _height)
+{
+	_container.innerHTML = '';
+	_container.appendChild(_append);
+	_container.appendChild(_fgtxt);
+	_append.style.animation = _anim;
+	_fgtxt.innerHTML = `Tekst, który zostanie wygrawerowany: ${_text}`;
+	_append.style.width = _width;
+	_append.style.height = _height;
 }
